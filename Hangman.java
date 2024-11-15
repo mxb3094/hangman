a52d8945f3759a7a94cd1e83f2de52d0d90483e1
Lewis756 <144144855+Lewis756@users.noreply.github.com>
2024-11-15 19:02:40 +0000

    current game

--- Hangman.java
+++ Hangman.java
@@ -23,7 +23,8 @@ public class Hangman
    }
 
    Random random = new Random();
-   setWordGuess(
+   setWordGuess(wordbankused.get(random.nextInt(wordbankused.size())));
+   setGuessCurrent(new StringBuilder("_".repeat(getWordGuess().length())));
 
 
 }
@@ -45,7 +46,7 @@ private void setGuessCurrent(StringBuilder GuessCurrent)
     this.GuessCurrent = GuessCurrent;
 }
 
-public int RemainingAttempts()
+public int getRemainingAttempts()
 {
     return RemainingAttempts;
 }
@@ -56,5 +57,47 @@ private void setRemainingAttempts( int RemainingAttempts)
 
 public void start()
 {
-
-}
+  if (getWordGuess()==null) //meaning its empty
+  {
+    System.out.println("The game cannot start because missing list");
+    return;
+  }
+  try (Scanner scanner = new Scanner (System.in))
+  {
+    while(getRemainingAttempts() > 0 && getGuessCurrent().indexOf("_") >= 0)
+    {
+        System.out.println("The current guess is " + getGuessCurrent());
+        //shows the current guess
+        System.out.println("The attempts remaining are:"+getRemainingAttempts());
+        //this shows the attemps remaing
+        System.out.println("Please enter a letter:");
+        char attempt = scanner.nextLine().charAt(0);
+        //begins to check guess
+        if (getWordGuess().contains(String.valueOf(attempt)))
+        {
+            for (int i = 0; i< getWordGuess().length(); i++)
+            {
+                if(getWordGuess().charAt(i) == attempt )
+                {
+                    //sets to attempt
+                    getGuessCurrent().setCharAt(i,attempt)
+                }
+            }
+        }else{
+            setRemainingAttempts(getRemainingAttempts()-1);
+            //if not chosen correctly
+            System.out.println("Incorrect guess attempt");
+        }
+    }
+    //now to see if they won 
+    if(getGuessCurrent().indexOf("_") < 0)
+    {
+        System.out.printlin("Congratsss You Won! our game of Hangman"+ getWordGuess());
+        //attempts if they won
+    }else{
+        System.out.println("Game is over the word was "+ getWordGuess());
+      }
+ //ran out of attempts, did not win
+    }
+   }
+  }
