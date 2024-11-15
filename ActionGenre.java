ddee2a54a2d799d2461a2b9823d8f6a612a0d8a2
mxb3094 <151090912+mxb3094@users.noreply.github.com>
2024-11-15 11:54:28 -0600

    Update ActionGenre.java

--- ActionGenre.java
+++ ActionGenre.java
@@ -1,14 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.FileReader;
 import java.util.List;
 import java.util.ArrayList;
 
 public class ActionGenre implements Genre
 {
-    private List <String> WordBankUsed;
+    private List<String> wordbankused;
+
+    //method to read words from file
+    private void LoadWordsUsedFromFile()
+    {
+         try (BufferedReader reader = new BufferedReader(new FileReader("ActionGenre.txt")))
+          {  
+            String line;
+            while ((line = reader.readLine() != null))
+            {
+                line = tine.trim(); 
+                // good for presentation 
+                //make sure to check line is not empty
+                if(!line.isEmpty())
+                {
+                    wordbankused.add(line);
+                }
+            }
+            System.out.println("Words know being played from ActionGenre.txt");
+          } catch(IOException e)
+          {
+            System.out.println("Error was not able to read words from ActionGenre.txt")
+          } 
+    }
+    //Action GENRE method
     public ActionGenre()
     {
-        
+        wordbankused = new ArrayList<>();
+        LoadWordsUsedFromFile();
+
     }
-}
+    //specific methos implementation for this class
+    public List<String> getWordsUsed()
+    {
+        return wordbankused;
+    }
